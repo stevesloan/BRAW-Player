dbf942265d59c09c95167b96bafcebefb41d598d
Steve Sloan <steve@stevesloan.ca>
2019-07-16 12:09:16 -0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,226 @@
+#include "mainwindow.h"
+#include "BlackmagicRawAPI.h"
+#include <QApplication>
+
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#ifdef DEBUG
+    #include <cassert>
+    #define VERIFY(condition) assert(SUCCEEDED(condition))
+#else
+    #define VERIFY(condition) condition
+#endif
+
+static const BlackmagicRawResourceFormat s_resourceFormat = blackmagicRawResourceFormatRGBAU8;
+static const int s_maxJobsInFlight = 3;
+static std::atomic<int> s_jobsInFlight = {0};
+
+struct UserData
+{
+    unsigned long long frameIndex;
+};
+
+class CameraCodecCallback : public IBlackmagicRawCallback
+{
+public:
+    explicit CameraCodecCallback() = default;
+    virtual ~CameraCodecCallback() = default;
+
+    virtual void ReadComplete(IBlackmagicRawJob* readJob, HRESULT result, IBlackmagicRawFrame* frame)
+    {
+        UserData* userData = nullptr;
+        VERIFY(readJob->GetUserData((void**)&userData));
+
+        IBlackmagicRawJob* decodeAndProcessJob = nullptr;
+
+        if (result == S_OK)
+            VERIFY(frame->SetResourceFormat(s_resourceFormat));
+
+        if (result == S_OK)
+            result = frame->CreateJobDecodeAndProcessFrame(nullptr, nullptr, &decodeAndProcessJob);
+
+        if (result == S_OK)
+            VERIFY(decodeAndProcessJob->SetUserData(userData));
+
+        if (result == S_OK)
+            result = decodeAndProcessJob->Submit();
+
+        if (result != S_OK)
+        {
+            if (decodeAndProcessJob)
+                decodeAndProcessJob->Release();
+
+            delete userData;
+        }
+
+        readJob->Release();
+    }
+
+    virtual void ProcessComplete(IBlackmagicRawJob* job, HRESULT result, IBlackmagicRawProcessedImage*)
+    {
+        UserData* userData = nullptr;
+        VERIFY(job->GetUserData((void**)&userData));
+
+        std::cout << "Processed frame index: " << userData->frameIndex << std::endl;
+        delete userData;
+
+        job->Release();
+        --s_jobsInFlight;
+    }
+
+    virtual void DecodeComplete(IBlackmagicRawJob*, HRESULT) {}
+    virtual void TrimProgress(IBlackmagicRawJob*, float) {}
+    virtual void TrimComplete(IBlackmagicRawJob*, HRESULT) {}
+    virtual void SidecarMetadataParseWarning(IBlackmagicRawClip*, const char*, uint32_t, const char*) {}
+    virtual void SidecarMetadataParseError(IBlackmagicRawClip*, const char*, uint32_t, const char*) {}
+
+    virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID, LPVOID*)
+    {
+        return E_NOTIMPL;
+    }
+
+    virtual ULONG STDMETHODCALLTYPE AddRef(void)
+    {
+        return 0;
+    }
+
+    virtual ULONG STDMETHODCALLTYPE Release(void)
+    {
+        return 0;
+    }
+};
+
+HRESULT ProcessClip(IBlackmagicRawClip* clip)
+{
+    HRESULT result;
+
+    long unsigned int frameCount = 0;
+    long unsigned int frameIndex = 0;
+
+    result = clip->GetFrameCount(&frameCount);
+
+    while (frameIndex < frameCount)
+    {
+        if (s_jobsInFlight >= s_maxJobsInFlight)
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
+            continue;
+        }
+
+        IBlackmagicRawJob* jobRead = nullptr;
+        if (result == S_OK)
+            result = clip->CreateJobReadFrame(frameIndex, &jobRead);
+
+        UserData* userData = nullptr;
+        if (result == S_OK)
+        {
+            userData = new UserData();
+            userData->frameIndex = frameIndex;
+            VERIFY(jobRead->SetUserData(userData));
+        }
+
+        if (result == S_OK)
+            result = jobRead->Submit();
+
+        if (result != S_OK)
+        {
+            if (userData != nullptr)
+                delete userData;
+
+            if (jobRead != nullptr)
+                jobRead->Release();
+
+            break;
+        }
+
+        ++s_jobsInFlight;
+
+        frameIndex++;
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc > 2)
+        {
+            std::cerr << "Usage: " << argv[0] << " clipName.braw" << std::endl;
+            return 1;
+        }
+
+        const char* clipName = nullptr;
+        bool clipNameProvided = argc == 2;
+        if (clipNameProvided)
+        {
+            clipName = argv[1];
+        }
+        else
+        {
+            clipName = "../../../Media/sample.braw";
+        }
+
+        HRESULT result = S_OK;
+
+        IBlackmagicRawFactory* factory = nullptr;
+        IBlackmagicRaw* codec = nullptr;
+        IBlackmagicRawClip* clip = nullptr;
+
+        CameraCodecCallback callback;
+
+        do
+        {
+            factory = CreateBlackmagicRawFactoryInstanceFromPath("./Libraries/");
+            if (factory == nullptr)
+            {
+                std::cerr << "Failed to create IBlackmagicRawFactory!" << std::endl;
+                result = E_FAIL;
+                break;
+            }
+
+            result = factory->CreateCodec(&codec);
+            if (result != S_OK)
+            {
+                std::cerr << "Failed to create IBlackmagicRaw!" << std::endl;
+                break;
+            }
+
+            result = codec->OpenClip(clipName, &clip);
+            if (result != S_OK)
+            {
+                std::cerr << "Failed to open IBlackmagicRawClip!" << std::endl;
+                break;
+            }
+
+            result = codec->SetCallback(&callback);
+            if (result != S_OK)
+            {
+                std::cerr << "Failed to set IBlackmagicRawCallback!" << std::endl;
+                break;
+            }
+
+            result = ProcessClip(clip);
+            codec->FlushJobs();
+
+        } while(0);
+
+        if (clip != nullptr)
+            clip->Release();
+
+        if (codec != nullptr)
+            codec->Release();
+
+        if (factory != nullptr)
+            factory->Release();
+
+
+    //////////
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ ui_mainwindow.h
@@ -0,0 +1,72 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.13.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGraphicsView *graphicsView;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(400, 300);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        graphicsView = new QGraphicsView(centralWidget);
+        graphicsView->setObjectName(QString::fromUtf8("graphicsView"));
+        graphicsView->setGeometry(QRect(80, 30, 256, 192));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 27));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
